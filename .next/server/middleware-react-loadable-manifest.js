53f8316d1465106602db9dcb914d565c125dcb29
welliton gervickas <wellitogervickas@gmail.com>
2022-11-25 17:07:05 -0300

    fix: remove node modules

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"../components/shared/Wallet/Connect.tsx -> @components/shared/Modal":{"id":"../components/shared/Wallet/Connect.tsx -> @components/shared/Modal","files":["static/chunks/components_shared_Modal_tsx.js"]}}
